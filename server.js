c54c20f5948f2331a9317c11f4fce1326bac999a
Adam Dienes <hello@adamdienes.com>
2024-04-05 12:54:53 +0200

    Add /product/:barcode logic, image_url

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const API_TOKEN = process.env.API_TOKEN || '123456';
+const API_TOKEN = process.env.API_TOKEN || '9sOlQBvayE';
 const dbPath = './database.sqlite';
 
 app.use(cors()); // CORS needed for all routes
@@ -45,6 +45,7 @@ db.serialize(() => {
         id INTEGER PRIMARY KEY,
         name TEXT,
         description TEXT,
+        image_url TEXT,
         barcode TEXT
     )`);
 
@@ -67,9 +68,9 @@ db.serialize(() => {
         FOREIGN KEY(ingredientId) REFERENCES Ingredients(id)
     )`);
 
-     // Mock-up data 
-     // TODO: Add more relevant data
-     db.run(`INSERT INTO Products (name) VALUES ('Product 11')`, (err) => {
+    // Mock-up data 
+    // TODO: Add more relevant data
+    db.run(`INSERT INTO Products (name, description, barcode, image_url) VALUES (?, ?, ?, ?)`, ['Product 1', 'Lorem ipsum', '1122', 'https://picsum.photos/200'], (err) => {
         if (err) {
             console.error('Error inserting product:', err.message);
         }
@@ -81,7 +82,16 @@ db.serialize(() => {
         }
     });
 
-    db.run(`INSERT INTO Ingredients (name) VALUES ('Ingredient A')`, (err) => {
+    // Inserting an ingredient with boolean values
+    // TODO: Add all boolean attributes (isDairyFree, etc.)
+    db.run(`INSERT INTO Ingredients (name, isVegan, isGlutenFree) VALUES (?, ?, ?)`, ['Tej', 1, 1], (err) => {
+        if (err) {
+            console.error('Error inserting ingredient:', err.message);
+        }
+    });
+
+    // Inserting an ingredient with boolean values
+    db.run(`INSERT INTO Ingredients (name, isVegan, isGlutenFree) VALUES (?, ?, ?)`, ['Sajt', 1, 0], (err) => {
         if (err) {
             console.error('Error inserting ingredient:', err.message);
         }
@@ -119,17 +129,44 @@ app.get('/products', (req, res) => {
 });
 
 // Get product by barcode 
-app.get('/products/:barcode', (req, res) => {
+// MIN function is used to get the minimum value of boolean attributes (false < true)
+// If any ingredient has false value for an attribute, the product is not safe to consume
+
+app.get('/product/:barcode', (req, res) => {
     const barcode = req.params.barcode;
-    db.get(`SELECT * FROM Products WHERE barcode = ?`, [barcode], (err, row) => {
+    db.get(`SELECT p.*, 
+                    GROUP_CONCAT(i.name) AS ingredients,
+                    MIN(i.isVegan) AS isVegan,
+                    MIN(i.isGlutenFree) AS isGlutenFree,
+                    MIN(i.isDairyFree) AS isDairyFree,
+                    MIN(i.isNutFree) AS isNutFree,
+                    MIN(i.isVegetarian) AS isVegetarian,
+                    MIN(i.isHalal) AS isHalal,
+                    MIN(i.isKosher) AS isKosher
+            FROM Products p 
+            LEFT JOIN ProductIngredients pi ON p.id = pi.productId
+            LEFT JOIN Ingredients i ON pi.ingredientId = i.id
+            WHERE p.barcode = ?
+            GROUP BY p.id`, [barcode], (err, row) => {
         if (err) {
             return res.status(500).json({ error: err.message });
         }
+        if (!row) {
+            return res.status(404).json({ error: 'Product not found (Barcode: ' + barcode + ')'});
+        }
+        
+        if (row.ingredients) {
+            row.ingredients = row.ingredients.split(',');
+        } else {
+            row.ingredients = [];
+        }
+        
         res.json(row);
     });
 });
 
+
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on localhost:${PORT}`);
 });
