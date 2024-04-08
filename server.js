fea3b0f5ec5baabc204a7057738820298a6bde88
Adam Dienes <hello@adamdienes.com>
2024-04-08 20:24:13 +0200

    Added CORS and API token validation message

--- server.js
+++ server.js
@@ -8,17 +8,20 @@ const app = express();
 const PORT = process.env.PORT;
 const API_TOKEN = process.env.API_TOKEN;
 
+app.use(cors());
+
 // Middleware to check API token
 const checkToken = (req, res, next) => {
     const token = req.header('Authorization');
-    if (!token || token !== `Bearer ${API_TOKEN}`) {
+    if (!token) {
         return res.status(401).json({ error: 'Unauthorized' });
+    }
+    if (token !== `Bearer ${API_TOKEN}`){
+        return res.status(401).json({ error: 'Invalid token' });
     }
     next();
 };
-
 app.use(checkToken);
-app.use(cors()); // CORS needed for all routes
 
 // Initialize database and routes
 app.use('/', routes);
